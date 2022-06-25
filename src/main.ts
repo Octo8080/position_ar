f9b76b74bf23953f768c34a658c1d2eec4c79fb3
Octo8080 <okutannderuta0529@gmail.com>
2022-06-25 21:58:48 +0900

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {
 } from "./canvas.ts";
 import { orientationHandler } from "./device_orientation.ts";
 import { positionHundler, setTarget } from "./position.ts";
-
+import {initialTarget} from "./api.ts"
 window.onload = async () => {
   if (!navigator.geolocation) return;
   setInterval(positionHundler, 1000);
@@ -23,34 +23,3 @@ window.onload = async () => {
 };
 
 window.addEventListener("deviceorientationabsolute", orientationHandler, true);
-
-async function initialTarget():Position {
-  const result = await fetch("/api/position")
-  const resultJson = result.json()
-  if(!isPosition(resultJson)) throw new Error("Result is not Position")
-  return resultJson
-  
-}
-
-interface Position{
-  latitude: number;
-  longitude: number;
-  altitude: number;
-}
-
-function isPosition(lawArg: unknown): lawArg is Position{
-  if(!lawArg) return false
-
-  const arg = lawArg as {[key:string]:unknown}
-
-  if("latitude" in arg) return false
-  if(typeof arg.latitude === "number") return false
-  
-  if("longitude" in arg) return false
-  if(typeof arg.longitude === "number") return false
-  
-  if("altitude" in arg) return false
-  if(typeof arg.altitude === "number") return false
-
-  return true
-} 
